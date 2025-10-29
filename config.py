462a6838c67e7342a8ed6c38a8279df9473a9b9e
Knut Ola Dølven <kdo000@uit.no>
2025-10-29 15:00:19 +0100

    Added config file

---
+++ config.py
@@ -0,0 +1,13 @@
+'''
+Configuration settings for the ML_Flare_Flow scripts.
+
+Author: Knut Ola Dølven
+'''
+
+from pathlib import Path
+
+# Base directory of the project
+PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
+DATA_DIR = PROJECT_ROOT / 'data'
+
+
